bb46fcef57d5209c60bcb374ec2d00877e1f35dc
travdet <93493656+travdet@users.noreply.github.com>
2025-10-31 11:10:01 -0400

    Use JavaScript OpenNext config

---
+++ open-next.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+};
